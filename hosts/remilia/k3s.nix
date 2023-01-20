f8febe286150ff496fd11e092ed3c575c494570e
SnO2WMaN <me@sno2wman.net>
2023-01-20 11:11:41 +0900

    Update k3s permission

--- hosts/remilia/k3s.nix
+++ hosts/remilia/k3s.nix
@@ -1,6 +1,9 @@
 {config, ...}: {
+  networking.firewall.allowedTCPPorts = [6443];
+
   services.k3s = {
     enable = true;
     role = "server";
+    extraFlags = "--write-kubeconfig-mode 644";
   };
 }
