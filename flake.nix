0bc9147e7d68834a80742d3279660ccfc0e23985
SnO2WMaN <me@sno2wman.net>
2023-01-24 04:58:39 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,11 +7,6 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-    arion = {
-      url = "github:hercules-ci/arion";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
       url = "github:ryantm/agenix";
@@ -21,10 +16,6 @@
       url = "github:msteen/nixos-vscode-server";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # vscode-marketplace = {
-    #   url = "github:AmeerTaweel/nix-vscode-marketplace";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -33,11 +24,10 @@
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
-    yamlfmt = {
-      url = "github:SnO2WMaN/yamlfmt.nix";
-      inputs.devshell.follows = "devshell";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+    # vscode-marketplace = {
+    #   url = "github:AmeerTaweel/nix-vscode-marketplace";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
   outputs = {
     self,
@@ -47,9 +37,6 @@
   } @ inputs:
     {
       nixosConfigurations = import ./hosts inputs;
-      # homeConfigurations = import ./home-manager inputs;
-
-      # overlays.default = import ./pkgs/overlay.nix;
     }
     // flake-utils.lib.eachDefaultSystem (
       system: let
@@ -58,9 +45,6 @@
           overlays = with inputs; [
             devshell.overlay
             agenix.overlay
-            (final: prev: {
-              yamlfmt = yamlfmt.packages.${system}.yamlfmt;
-            })
           ];
         };
       in {
@@ -72,27 +56,7 @@
             git-crypt
             taplo-cli
             agenix
-            yamlfmt
           ];
-          commands = [
-            {
-              name = "my-nixos-build";
-              category = "nixos";
-              command = "nixos-rebuild build --flake \".#$(hostname)\"";
-            }
-            {
-              name = "my-nixos-switch";
-              category = "nixos";
-              command = "nixos-rebuild switch --flake \".#$(hostname)\"";
-            }
-          ];
-          # devshell.startup.pnpm_install.text = "pnpm install";
-          # env = [
-          #   {
-          #     name = "PATH";
-          #     prefix = "$PRJ_ROOT/node_modules/.bin";
-          #   }
-          # ];
         };
       }
     );
