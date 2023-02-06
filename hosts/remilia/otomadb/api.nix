23d4c5c64c7e89f4890c75990fd44f9f970d9be8
SnO2WMaN <me@sno2wman.net>
2023-02-06 22:00:01 +0900

    refactor(remilia): `api.otomadb.com`の修正

---
+++ hosts/remilia/otomadb/api.nix
@@ -0,0 +1,51 @@
+{...}: {
+  networking.firewall.allowedTCPPorts = [
+    8080
+    8443
+  ];
+
+  security.acme = {
+    acceptTerms = true;
+    certs = {
+      "api.otomadb.com" = {
+        email = "me@sno2wman.net";
+      };
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    upstreams = {
+      "api-otomadb-com" = {
+        servers = {
+          "0.0.0.0:30080" = {};
+        };
+      };
+    };
+
+    virtualHosts."api.otomadb.com" = {
+      enableACME = true;
+      forceSSL = true;
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 8080;
+        }
+        {
+          addr = "0.0.0.0";
+          port = 8443;
+          ssl = true;
+        }
+      ];
+      locations."/" = {
+        proxyPass = "http://api-otomadb-com/";
+      };
+    };
+  };
+}
